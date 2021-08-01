fd23645fec99c10d0d3650ad4379aa9b52b0fcf0
haossten <haossten@gmail.com>
2021-08-01 23:24:36 +0300

    Added support for uploading images

--- kievdevice/settings.py
+++ kievdevice/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .secret_settings import SECRET_KEY, DBNAME, DBUSER, DBPASSWORD, DBHOST, DBPORT
 
@@ -124,6 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_dev'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
